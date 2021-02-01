7f76250e92a7933eb9b54130db55234550c429b3
DaniloLima122 <limadanilo06@gmail.com>
2021-02-01 18:23:09 -0300

    deleting unusual directive

--- src/app/shared/directives/toggle-film-card.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToggleFilmCardDirective } from './toggle-film-card.directive';
-
-describe('ToggleFilmCardDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ToggleFilmCardDirective();
-    expect(directive).toBeTruthy();
-  });
-});
